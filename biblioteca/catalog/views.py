18f741d5e1590292416a37913b1cfd6f44ff4599
jorgeOmurillo <j.orlandomurillo@gmail.com>
2017-08-14 22:12:16 +0800

    login authentication

--- biblioteca/catalog/views.py
+++ biblioteca/catalog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from .models import Author, Book, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def index(request):
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
